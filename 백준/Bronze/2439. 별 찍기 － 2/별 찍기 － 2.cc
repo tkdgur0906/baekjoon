e7989ce96364f07cff39f3739abdb38b0e2d2384
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2022-03-07 20:23:43 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num = 0;
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num - i -1; j++) {
+			cout << " ";
+		}
+		for (int k = 0; k < i + 1; k++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
