9c93692610f4d22acdcf39c9d6d2d526a068adb5
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2022-03-08 12:13:05 +0900

    [Bronze III] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin >> c;
+	for (int i = 0; i < c; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a + b << endl;
+	}
+}
