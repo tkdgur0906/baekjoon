c8f828fb76e5a58a8a04a40e86b6108dc7de277e
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2022-03-08 12:03:39 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,temp;   
+    cin >> a >> b >> c;
+    if(a > b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if(b > c){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    if(a > b){
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    cout << a << " " << b << " " << c;
+}
