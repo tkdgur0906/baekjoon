24c6f8b03723918d3795b81c02a9924a29e24ef3
tkdgur0906 <39580658+tkdgur0906@users.noreply.github.com>
2022-03-14 20:06:01 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(void) {
-	int n,x,y;
+	int n,x,y,temp = 1;
 	int weight[50];
 	int height[50];
-	int score[50] = { 0, };
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
@@ -14,11 +13,10 @@ int main(void) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n ; j++) {
 			if (weight[i] < weight[j] && height[i] < height[j]) {
-				score[i]++;
+				temp++;
 			}
 		}
-	}
-	for (int i = 0; i < n; i++) {
-		cout << score[i] + 1 << ' ';
+		cout << temp << " ";
+		temp = 1;
 	}
 }
